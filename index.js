64391f163d12e77710115735c70c2ed737357273
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2025-08-03 20:57:32 +0100

    Add server creation logic

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  const pageName = req.url === "/" ? "index.html" : `${req.url}.html`;
+  const page = path.join(__dirname, "public", pageName);
+
+  fs.readFile(page, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content404) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content404, "utf8");
+          }
+        );
+      } else {
+        res.writeHead(200);
+        res.end(`Server error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content, "utf8");
+    }
+  });
+});
+
+server.listen(8080, () => {
+  console.log("Running on port 8080");
+});
